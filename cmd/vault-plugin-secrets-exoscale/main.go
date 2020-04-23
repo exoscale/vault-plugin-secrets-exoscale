588ba07c7f57d17e413d0d1fe1e4fcaffaec4f05
Marc Falzon <marc@exoscale.com>
2020-04-23 18:31:02 +0200

    Add missing files

---
+++ cmd/vault-plugin-secrets-exoscale/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	exoscale "github.com/exoscale/vault-plugin-secrets-exoscale"
+	"github.com/hashicorp/vault/api"
+	"github.com/hashicorp/vault/sdk/plugin"
+)
+
+func main() {
+	apiClientMeta := &api.PluginAPIClientMeta{}
+	flags := apiClientMeta.FlagSet()
+	if err := flags.Parse(os.Args[1:]); err != nil {
+		log.Fatalf("error parsing command line: %s", err)
+		os.Exit(1)
+	}
+
+	tlsConfig := apiClientMeta.GetTLSConfig()
+	tlsProviderFunc := api.VaultPluginTLSProvider(tlsConfig)
+
+	err := plugin.Serve(&plugin.ServeOpts{
+		BackendFactoryFunc: exoscale.Factory,
+		TLSProviderFunc:    tlsProviderFunc,
+	})
+	if err != nil {
+		log.Fatalf("shutting down: %s", err)
+		os.Exit(1)
+	}
+}
